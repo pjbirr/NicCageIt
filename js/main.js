77002be34bb5f77617be7f2af71baf0fd15e3a5a
Paul <herppfel@gmail.com>
2016-02-13 17:12:44 -0500

    more js

--- js/main.js
+++ js/main.js
@@ -10,22 +10,38 @@
 	canvas.height = HEIGHT;
 
 	var backplate = new Image();
-	//backplate.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8q03alOVXNFYe2t7HwuTX4KXdzBVuHOT6S8aM7X5pSM9L55g4";
-	//backplate.src = "/img/img.jpg";
-
-
-	document.getElementById("backplate-img").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8q03alOVXNFYe2t7HwuTX4KXdzBVuHOT6S8aM7X5pSM9L55g4";
 
 	//ctx.drawImage(backplate, 0, 0, backplate.width, backplate.height, 0, 0, WIDTH, HEIGHT);
-	ctx.fillText("Hello World", 20, 20);
+	ctx.font = "24px sans-serif";
+	ctx.fillText("No image uploaded.", WIDTH/2.65, HEIGHT/2);
 
-	$("#backplate-img").faceDetection({
-		complete: function (faces) {
-			console.log(faces);
-		},
-		error: function(code, message) {
-			console.log("Error : " + message);
-		}
+	$("#file-upload").change(function() {
+		console.log(this.files);
+
+		var reader = new FileReader();
+
+		reader.onload = function(e) {
+	        var img = new Image();
+
+	        img.onload = function() {
+	        	//img.width = WIDTH;
+	        	//img.height = HEIGHT;
+	            ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, WIDTH, HEIGHT);
+	        }
+	        	img.src = e.target.result;
+    	}
+    	
+    	reader.readAsDataURL(this.files[0]);
+
+		$("#canvas").faceDetection({
+			complete: function (faces) {
+				console.log(faces);
+			},
+			error: function(code, message) {
+				console.log("Error : " + message);
+			}
+		});
+	
 	});
 
 })();
